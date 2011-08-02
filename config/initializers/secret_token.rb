4c6a995e861b5c7f1b18ac92197a5c14ebcd6cf2
jsy <jeff@youel.net>
2011-08-02 09:47:33 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AaTestNestedModels::Application.config.secret_token = 'fdc09fc697771a0451cf027e6bfb9aba6dac65bcfd29d24d8720811746aaee3dd6c23baeaebe2f6265febbd2ac00c87444e79459e133b8c6b56f86abf9f84f42'
